855002e079e37d7af6cc6ef900c051eaedf8edf9
FrancoP505 <flpiedrabuena@gmail.com>
2025-09-21 21:22:26 -0300

    nuevo repo en react

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./styles/App.css"; // o el nombre correcto de tu CSS
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
